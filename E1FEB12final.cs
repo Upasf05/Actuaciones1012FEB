c394956cfe104954e4d219d24e7b2704f9439be1
Upasf05 <fcrams2003@gmail.com>
2025-02-16 19:50:26 -0500

    Update E1FEB12final.cs

--- E1FEB12final.cs
+++ E1FEB12final.cs
@@ -4,33 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EjercicioCalculadora
+namespace EjercicioCalculadora //Espacio principal del programa
 {
-    internal class Operacion
+    internal class Operacion //Clase interna para la realización de operaciones
     {
         public virtual int Operar()
         {
-            return 0; 
+            return 0; //Método base sin implementación específica
         }
     }
 
-    internal class Calculadora : Operacion
+    internal class Calculadora : Operacion //Clase interna que representa a la calculadora como un objeto
     {
-        public int numero1;
-        public int numero2;
+        public int numero1; //Definir primer número que realizará operación
+        public int numero2; //Definir segundo número que realizará operación
 
-        public Calculadora(int numero1, int numero2)
+        public Calculadora(int numero1, int numero2) //Método donde funcionará la calculadora
         {
-            this.numero1 = numero1;
-            this.numero2 = numero2;
+            this.numero1 = numero1; //Colcoar el primer valor en el método
+            this.numero2 = numero2; //Colocar el segundo valor en el método
         }
 
-        public override int Operar()
+        public override int Operar() //Usar polimorfismo en el objeto para permitir método similares
         {
-            return sumar(); 
+            return sumar(); //Por defecto, se realizará una suma
         }
 
-        public virtual int dividir()
+        public virtual int dividir() //Método de división
         {
             if (numero2 != 0)
                 return numero1 / numero2;
@@ -41,42 +41,42 @@ namespace EjercicioCalculadora
             }
         }
 
-        public virtual int multiplicar()
+        public virtual int multiplicar() //Método de multiplicación
         {
             return numero1 * numero2;
         }
 
-        public virtual int sumar()
+        public virtual int sumar() //Método de suma
         {
             return numero1 + numero2;
         }
 
-        public virtual int restar()
+        public virtual int restar() //Método de resta
         {
             return numero1 - numero2;
         }
     }
 
-    class Program
+    class Program //Clase abstracta que imprime los resultados, actúa como una interfaz
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Único método
         {
-            int numero1, numero2;
-            Console.WriteLine("Ingrese el primer número: ");
-            numero1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ingrese el segundo número: ");
-            numero2 = Convert.ToInt32(Console.ReadLine());
+            int numero1, numero2; //Definir los valores
+            Console.WriteLine("Ingrese el primer número: "); //Solicitar la impresión del primer número
+            numero1 = Convert.ToInt32(Console.ReadLine()); //Leer el número
+            Console.WriteLine("Ingrese el segundo número: "); //Solicitar la impresión del segundo número
+            numero2 = Convert.ToInt32(Console.ReadLine()); //Leer el segundo número
 
-            Operacion operacion = new Calculadora(numero1, numero2); 
+            Operacion operacion = new Calculadora(numero1, numero2); //Polimorfismo, convertir clase en valor a imprimir
 
-            Calculadora calculadora = new Calculadora(numero1, numero2);
+            Calculadora calculadora = new Calculadora(numero1, numero2); //Repetir el proceso de polimorfismo
 
-            Console.WriteLine($"Suma: {calculadora.sumar()}");
+            Console.WriteLine($"Suma: {calculadora.sumar()}"); //Imprimir lso valores
             Console.WriteLine($"Resta: {calculadora.restar()}");
             Console.WriteLine($"Multiplicación: {calculadora.multiplicar()}");
             Console.WriteLine($"División: {calculadora.dividir()}");
 
-            Console.ReadLine(); 
+            Console.ReadLine(); //Cerrar el método
         }
     }
 }
